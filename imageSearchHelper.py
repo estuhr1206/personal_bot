27748322738233abd6f65612b83bad34fa790701
explosion1206 <40578236+estuhr1206@users.noreply.github.com>
2021-05-25 13:53:11 -0400

    Add files via upload
    added functionality that reflects the usage of offset being fixed in the google images repo. This makes error checking for file size simpler and more comprehensive, as well as allowing for the random image search command to be used.

--- imageSearchHelper.py
+++ imageSearchHelper.py
@@ -10,5 +10,20 @@ def findImage(inputKeywords, num: int):
     paths = response.download(arguments)   #passing the arguments to the function
     print(paths)
 
-
+def findCountedImage(inputKeywords, num: int, randNum: int):
+    #inputKeywords can possibly come in as a list of words
+    response = google_images_download.googleimagesdownload()   #class instantiation
+    #space in those quotes makes a space between words in joined string
+    
+    arguments = {"keywords":inputKeywords,"limit":num,"print_urls":True, "no_directory":True, "offset":randNum}   #creating list of arguments
+    paths = response.download(arguments)   #passing the arguments to the function
+    print(paths)
+def testing():
+    response = google_images_download.googleimagesdownload()   #class instantiation
+    #space in those quotes makes a space between words in joined string
+    
+    arguments = {"keywords":'lawn chair',"limit":2,"print_urls":True, "no_directory":True, "offset":1}   #creating list of arguments
+    paths = response.download(arguments)   #passing the arguments to the function
+    print(paths)
+#testing()
 #asdf
